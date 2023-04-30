376fb6d54a7ece823d74ce3c64166196e177ceb8
6531503120 <6531503120@lamduan.mfu.ac.th>
2023-04-30 19:00:37 +0700

    Fixed

--- scr/Main.java
+++ scr/Main.java
@@ -1,8 +1,12 @@
 import javax.sql.rowset.serial.SerialDatalink;
-
 public class Main {
     public static void main(String[] args) {
-            
-        }
+        //ย้ายไปไว้ในเมน
+        Order order = new Order("123456", "XYZ-123", 2, 50.0, "123 Main St, Anytown, USA");
+        // Process payment and update order status to "Shipped"
+        Date deliveryDate = new Date();
+        String trackingNumber = "1234567890";
+        order.setDeliveryInfo(deliveryDate, trackingNumber); 
+    }
 }
 
